9d2a9e1ceefc8ea0bca100df3cb8f94843f8afd6
Pompiliys <48363197+Pompiliys@users.noreply.github.com>
2021-12-10 20:51:45 +0300

    Delete 12,14,14,15 directory

--- 12,14,14,15/Author.java
+++
@@ -1,9 +0,0 @@
-package com.pompi;
-import java.util.Comparator;
-
-public class Author implements Comparator<Book>{
-    @Override
-    public int compare(Book obj1, Book obj2) {
-        return obj1.getAuthor().compareTo(obj2.getAuthor());
-    }
-}
--- 12,14,14,15/Title.java
+++
@@ -1,9 +0,0 @@
-package com.pompi;
-import java.util.Comparator;
-
-public class Title implements Comparator<Book>{
-    @Override
-    public int compare(Book obj1, Book obj2) {
-        return obj1.getTitle().compareTo(obj2.getTitle());
-    }
-}
